ef7974b497da41a88fd5008c7aa9f699a7f5df8a
sashipuhan4543 <sashi.puhan4543@gmail.com>
2019-08-22 14:24:07 +0530

    removed beforeTest

--- src/test/java/com/xp8/util/appiumService.java
+++ src/test/java/com/xp8/util/appiumService.java
@@ -2,7 +2,7 @@
  * 							Starts an appium server on ip address and port specified..
  * 
  * 
- 
+ */
 
 
 package com.xp8.util;
@@ -38,7 +38,7 @@ public class appiumService {
 	public static double TESTCASES_INCREMENTER=1;
 
 
-	@BeforeSuite	
+	//@BeforeSuite	
 	public void startServer() {
 
 		System.out.println("<------------------------Started Appium Service---------------------------------->");
@@ -83,7 +83,7 @@ public class appiumService {
 	public void profileSelectionForQAAndNonQA() {
 
 
-		if(MainController.TEAM_BASED_LOGIN.contains("QA Team")) {
+		/*if(MainController.TEAM_BASED_LOGIN.contains("QA Team")) {
 
 			// AllQA.PRIMARYDEVMDN=AllQA.PRIMARYDEVMDN;
 			//AllQA.REFERENCEDEVMDN=AllQA.REFERENCEDEVMDN;
@@ -99,7 +99,7 @@ public class appiumService {
 
 
 			System.out.println(AllQA.PRIMARYDEVMDN +","+AllQA.REFERENCEDEVMDN +","+AllQA.NUMOFCALLS);	
-		}
+		}*/
 
 	}
 
@@ -215,4 +215,3 @@ public class appiumService {
 
 		}
 }
-*/
